d5618dbe150dbcb3a2939caf180e644fc1896445
awarns <itqa1@MAC14958.local>
2013-10-15 14:05:18 -0400

    Commission Options Done

--- features/Consultant/Profile/commission_options.feature
+++ features/Consultant/Profile/commission_options.feature
@@ -7,10 +7,6 @@ Feature: Commission Options
 Background:
   Given I am on Her Virtual Office
 
-  Scenario: Choose Check Option and Enter Info
-    When I choose the check option and fill out information
-    Then I should see my updated check option
-
   Scenario: Choose Direct Deposit and Enter Info
     When I choose the direct deposit option and fill out information
     Then I should see my updated direct deposit options
--- lib/pages/Consultant/profile/commission_options_page.rb
+++ lib/pages/Consultant/profile/commission_options_page.rb
@@ -2,24 +2,15 @@ class CommissionOptionsPage
   include PageObject
   include DataMagic
 
-  radio_button(:check, :id => "MasterContentBody1_radio_commission_option_0")
-  radio_button(:direct_deposit, :id => "MasterContentBody1_radio_commission_option_1")
-  text_field(:name_on_check, :id => "MasterContentBody1_check_name")
-  text_field(:bank_name, :id => "MasterContentBody1_DirectDeposit1_txt_us_bank_name")
-  text_field(:routing_number, :id => "MasterContentBody1_DirectDeposit1_txt_us_routing_number")
-  text_field(:account_number, :id => "ctl00$MasterContentBody1$DirectDeposit1$txt_us_account_number")
-  text_field(:confirm_account_number, :id => "ctl00$MasterContentBody1$DirectDeposit1$txt_us_account_number_confirm")
-  select_list(:account_type, :id => "MasterContentBody1_DirectDeposit1_ddl_us_account_type")
+
+  text_field(:bank_number, :id => "MasterContentBody1_DirectDeposit1_txt_can_bank_number")
+  text_field(:branch_number, :id => "MasterContentBody1_DirectDeposit1_txt_can_branch_number")
+  text_field(:account_number, :id => "ctl00$MasterContentBody1$DirectDeposit1$txt_can_account_number")
+  text_field(:confirm_account_number, :id => "ctl00$MasterContentBody1$DirectDeposit1$txt_can_account_number_confirm")
+  text_field(:name_on_account, :id => "MasterContentBody1_DirectDeposit1_txt_can_name")
   link(:update_profile, :id => "MasterContentBody1_btn_update")
   link(:my_website_info, :text => "My Website Information")
 
-  def enter_check_info(data = {})
-
-    select_check
-    populate_page_with data_for(:enter_check_info, data)
-    update_profile
-
-  end
 
   def verify_check_options
 
@@ -29,15 +20,15 @@ class CommissionOptionsPage
 
   def enter_direct_deposit_info(data = {})
 
-    select_direct_deposit
     populate_page_with data_for(:enter_direct_deposit, data)
     update_profile
+
   end
 
   def verify_direct_deposit_info
 
-    self.bank_name.should == "Wells Fargo"
-    self.routing_number.should == "314074269"
+    self.bank_number.should == "011"
+    self.branch_number.should == "01002"
 
   end
 
